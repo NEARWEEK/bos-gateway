8866176a98e8ed5a9312e1d4d2ee7eac0aad5eed
rostyslav-dzhuman <drm2drm@ukr.net>
2024-04-19 12:33:44 +0300

    Embed BOS widgets

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import "error-polyfill";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@near-wallet-selector/modal-ui/styles.css";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import "react-bootstrap-typeahead/css/Typeahead.bs5.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "App.scss";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import EmbedPage from "./pages/EmbedPage";
+import { sanitizeUrl } from "@braintree/sanitize-url";
+import { useInitNear } from "near-social-vm";
+
+function App() {
+  const { initNear } = useInitNear();
+
+  useEffect(() => {
+    initNear &&
+      initNear({
+        networkId: "mainnet",
+        customElements: {
+          Link: (props) => {
+            if (!props.to && props.href) {
+              props.to = props.href;
+              delete props.href;
+            }
+            if (props.to) {
+              props.to = sanitizeUrl(
+                typeof props.to === "string" ? props.to : ""
+              );
+            }
+            return <Link {...props} />;
+          },
+        },
+        config: {
+          defaultFinality: undefined,
+        },
+      });
+  }, [initNear]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path={"/"}>
+            <EmbedPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/EmbedPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Widget } from "near-social-vm";
+
+export default function EmbedPage() {
+  return (
+    <div className="d-inline-block position-relative overflow-hidden w-100">
+      <Widget src="nearweekapp.near/widget/nearweek.com" props={{}} />
+    </div>
+  );
+}
